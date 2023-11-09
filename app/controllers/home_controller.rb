28e60b193c84f657205d366a9e874615921ed7b0
Sam Loecke <sloecke@uiowa.edu>
2023-11-09 13:06:50 -0600

    Deleted unnessecary items, added Cole's item model function to avoid merge conflict

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,9 @@ class HomeController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
 
   def index
-    # Fetch all categories. You would need a Category model for this,
-    # or you can remove this line if you don't have different categories.
-    @categories = Category.all if defined?(Category)
-    # TODO fix this to use the database categories
-    # Should get up to ten categories from the database
-    @categories = [{:id => 1, :name => "Category 1"}, {:id => 2, :name => "Category 2"}, {:id => 2, :name => "Category 3"}, {:id => 2, :name => "Category 4"}, {:id => 2, :name => "Category 5"}]
+
+    # Fetch all the categories from the database
+    @categories = Category.all.limit(10)
 
     # Fetch suggested items for sale, which might be a curated list based on some logic
     # Replace the 'suggested_items' method with the actual logic you want to use.
